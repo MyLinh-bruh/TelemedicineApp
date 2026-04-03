99032e3464ad8d58c0dd798646825ae6cdcade04
Phuong Thao <phuongthaodn.1005@gmail.com>
2026-04-03 17:01:05 +0700

    chore: setup Hilt Application and update AndroidManifest

--- app/src/main/java/com/example/telemedicineapp/TelemedicineApplication.kt
+++ app/src/main/java/com/example/telemedicineapp/TelemedicineApplication.kt
@@ -4,9 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TelemedicineApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        // Sau này có thể khởi tạo Timber logging hoặc notification channel ở đây
-    }
-}
+class TelemedicineApplication : Application()
