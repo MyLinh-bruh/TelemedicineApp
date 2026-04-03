cdc2d6918a4b8c42c21b5713caf1abe776d0b620
Phuong Thao <phuongthaodn.1005@gmail.com>
2026-04-03 15:12:22 +0700

    feat: add Doctor data model

--- app/src/main/java/com/example/telemedicineapp/model/Doctor.kt
+++ app/src/main/java/com/example/telemedicineapp/model/Doctor.kt
@@ -1,10 +1,12 @@
 package com.example.telemedicineapp.model
 
 data class Doctor(
-        val id: Int,
-        val name: String,
-        val specialty: String,
-        val rating: Double,
-        val imageUrl: String,
-        val description: String
-    )
+    val id: Int,
+    val name: String,
+    val specialty: String,
+    val rating: Double,
+    val imageUrl: String,
+    val description: String,
+    val address: String,
+    val hospitalName: String
+)
