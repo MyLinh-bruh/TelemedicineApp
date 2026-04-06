98b5d9e6aa31f4cfa2d29c86c70d51ca537024b8
ynnhan <luongyennhanlyn05@gmail.com>
2026-04-06 15:04:30 +0700

    Hoàn thiện tính năng Hồ sơ bệnh án và Đặt lịch hẹn

---
+++ app/src/main/java/com/example/telemedicineapp/presentation/screen/patient/PatientMedicalViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.telemedicineapp.presentation.screen.patient
+
+class PatientMedicalViewModel {
+}
---
+++ app/src/main/java/com/example/telemedicineapp/ui/screens/PatientMedicalRecordScreen.kt
@@ -0,0 +1,4 @@
+package com.example.telemedicineapp.ui.screens
+
+class PatientMedicalRecordScreen {
+}
